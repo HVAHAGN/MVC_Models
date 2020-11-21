cc436e0068b0313bda2871bba106cf9320b12f2d
hvahagn <vhoagamo@gmail.com>
2020-11-21 18:28:20 +0400

    Add project files.

---
+++ 001.Models/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _001.Models;
+
+namespace _001.Controlles
+{
+    public class HomeController : Controller
+    {
+        //public string Index()
+        //{
+        //    ThickModel model = new ThickModel("CBS", 100, 6000);
+        //    int totalSalary = model.TotalSalary();
+        //    string company = $"Company: {model.Company}\n";
+        //    string empoyees = $"Employees: {model.Employees}\n";
+        //    string salary = $"Total Salary: {totalSalary}\n";
+        //    return company + empoyees + salary;
+        //}
+
+        public string Index()
+        {
+            PlainModel model = new PlainModel()
+            {
+                Company = "Microsoft",
+                Employees = 100000,
+                Salary = 10000
+            };
+
+            int totalSalary = model.Employees * model.Salary;
+            string company = $"Company: {model.Company}\n";
+            string empoyees = $"Employees: {model.Employees}\n";
+            string salary = $"Total Salary: {totalSalary}\n";
+            return company + empoyees + salary;
+        }
+    }
+}
